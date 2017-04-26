ac838bca3f0d718b9b2aba97aff0dae4a4fe4c84
iissnan <iissnan@gmail.com>
2017-04-26 16:03:35 +0800

    Update Goals Controller

--- app/controllers/api/v1/goals_controller.rb
+++ app/controllers/api/v1/goals_controller.rb
@@ -4,8 +4,14 @@ class Api::V1::GoalsController < ApplicationController
   end
 
   def create
-    @goal = Goal.create!(goal_params)
-    json_response(@goal, :created)
+    begin
+      Match.find(params[:match_id])
+      Team.find(params[:team_id])
+      @goal = Goal.create!(goal_params)
+      render json: @goal, root: :data, status: :created
+    rescue ActiveRecord::RecordNotFound
+      render json: { message: 'Invalid Match or Team' }, status: :bad_request
+    end
   end
 
   def show
@@ -26,5 +32,6 @@ class Api::V1::GoalsController < ApplicationController
     params
         .require(:goal)
         .permit(:match_id, :team_id, :score, :at)
+        .merge(match_id: params[:match_id])
   end
 end
